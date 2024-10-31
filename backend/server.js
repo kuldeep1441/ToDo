15372a402b1f15cd64e4efbbd64a26142147323e
Kuldeep <kuldeepsinghtanwar2001@gmail.com>
2024-10-31 21:41:00 +0530

    correction 5 deployment

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,25 @@ dotenv.config();
 connectDB();
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: ["https://to-do-app-seven-delta-70.vercel.app/"],
-//     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
-//     credentials: true,
-//   })
-// );
-
 app.use(
   cors({
-    origin: '*', // Allow this origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
-    credentials: true, // Allow credentials (cookies, authorization headers)
+    origin: ["https://to-do-app-seven-delta-70.vercel.app/"],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    credentials: true,
   })
 );
 
+// app.use(
+//   cors({
+//     origin: '*', // Allow this origin
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+//     credentials: true, // Allow credentials (cookies, authorization headers)
+//   })
+// );
 
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(bodyParser.json({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/tasks", taskRoutes);
 
