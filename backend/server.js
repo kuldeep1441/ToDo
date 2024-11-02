51211da942173f74aa51a33888cdb3d825965d91
Kuldeep <kuldeepsinghtanwar2001@gmail.com>
2024-11-02 15:19:19 +0530

    server commit

--- backend/server.js
+++ backend/server.js
@@ -9,19 +9,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://to-do-app-seven-delta-70.vercel.app"],
+    origin: ["*"],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
 );
 
-// app.use(
-//   cors({
-//     origin: '*', // Allow this origin
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
-//     credentials: true, // Allow credentials (cookies, authorization headers)
-//   })
-// );
 
 
 // app.use(bodyParser.json({ extended: true }));
